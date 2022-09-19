1189864e5225da4cee944ffca3211ad2b96f9b83
DK Moon <dkmoon@gmail.com>
2022-09-20 02:48:21 +0900

    TCP server/client 추가

---
+++ tcpclient/tcpclient.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+// ws2_32.lib 를 링크한다.
+#pragma comment(lib, "Ws2_32.lib")
+
+static unsigned short SERVER_PORT = 27015;
+
+int main()
+{
+    int r = 0;
+
+    // Winsock 을 초기화한다.
+    WSADATA wsaData;
+    r = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (r != NO_ERROR) {
+        std::cerr << "WSAStartup failed with error " << r << std::endl;
+        return 1;
+    }
+
+    struct sockaddr_in serverAddr;
+
+    // TCP socket 을 만든다.
+    SOCKET sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sock == INVALID_SOCKET) {
+        std::cerr << "socket failed with error " << WSAGetLastError() << std::endl;
+        return 1;
+    }
+
+    // TCP 는 연결 기반이다. 서버 주소를 정하고 connect() 로 연결한다.
+    // connect 후에는 별도로 서버 주소를 기재하지 않고 send/recv 를 한다.
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(SERVER_PORT);
+    inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
+    r = connect(sock, (sockaddr*)&serverAddr, sizeof(serverAddr));
+    if (r == SOCKET_ERROR) {
+        std::cerr << "connect failed with error " << WSAGetLastError() << std::endl;
+        return 1;
+    }
+
+    // send 로 데이터를 보낸다. 여기서는 초기화되지 않은 쓰레기 데이터를 보낸다.
+    char data[32768];
+    r = send(sock, data, sizeof(data), 0);
+    if (r == SOCKET_ERROR) {
+        std::cerr << "send failed with error " << WSAGetLastError() << std::endl;
+        return 1;
+    }
+
+    std::cout << "Sent " << r << " bytes" << std::endl;
+
+    // Socket 을 닫는다.
+    r = closesocket(sock);
+    if (r == SOCKET_ERROR) {
+        std::cerr << "closesocket failed with error " << WSAGetLastError() << std::endl;
+        return 1;
+    }
+
+    // Winsock 을 정리한다.
+    WSACleanup();
+    return 0;
+}
---
+++ tcpserver/tcpserver.cpp
@@ -0,0 +1,93 @@
+﻿#include <cstring>
+#include <iostream>
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+// ws2_32.lib 를 링크한다.
+#pragma comment(lib, "Ws2_32.lib")
+
+static unsigned short SERVER_PORT = 27015;
+
+int main()
+{
+    int r = 0;
+
+    // Winsock 을 초기화한다.
+    WSADATA wsaData;
+    r = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (r != NO_ERROR) {
+        std::cerr << "WSAStartup failed with error " << r << std::endl;
+        return 1;
+    }
+
+    // TCP socket 을 만든다.
+    SOCKET passiveSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (passiveSock == INVALID_SOCKET) {
+        std::cerr << "socket failed with error " << WSAGetLastError() << std::endl;
+        return 1;
+    }
+
+    // socket 을 특정 주소, 포트에 바인딩 한다.
+    struct sockaddr_in serverAddr;
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(SERVER_PORT);
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    r = bind(passiveSock, (sockaddr*)&serverAddr, sizeof(serverAddr));
+    if (r == SOCKET_ERROR) {
+        std::cerr << "bind failed with error " << WSAGetLastError() << std::endl;
+        return 1;
+    }
+
+    // TCP 는 연결을 받는 passive socket 과 실제 통신을 할 수 있는 active socket 으로 구분된다.
+    // passive socket 은 socket() 뒤에 listen() 을 호출함으로써 만들어진다.
+    // active socket 은 passive socket 을 이용해 accept() 를 호출함으로써 만들어진다.
+    r = listen(passiveSock, 10);
+    if (r == SOCKET_ERROR) {
+        std::cerr << "listen faijled with error " << WSAGetLastError() << std::endl;
+        return 1;
+    }
+
+    // passive socket 을 이용해 accept() 로 연결을 기다린다.
+    // 연결이 완료되고 만들어지는 소켓은 active socket 이다.
+    std::cout << "Waiting for a connection" << std::endl;
+    struct sockaddr_in clientAddr;
+    int clientAddrSize = sizeof(clientAddr);
+    SOCKET activeSock = accept(passiveSock, (sockaddr*)&clientAddr, &clientAddrSize);
+    if (activeSock == INVALID_SOCKET) {
+        std::cerr << "accept failed with error " << WSAGetLastError() << std::endl;
+        return 1;
+    }
+
+    // socket 으로부터 데이터를 받는다.
+    // TCP 는 연결 기반이므로 누가 보냈는지는 accept 시 결정되고 그 뒤로는 send/recv 만 호출한다.
+    std::cout << "Receiving stream" << std::endl;
+    char buf[65536 * 2];
+    r = recv(activeSock, buf, sizeof(buf), 0);
+    if (r == SOCKET_ERROR) {
+        std::cerr << "recv failed with error " << WSAGetLastError() << std::endl;
+        return 1;
+    }
+
+    std::cout << "Received " << r << " bytes" << std::endl;
+
+    // 이 특정 연결의 activeSock 을 닫는다.
+    r = closesocket(activeSock);
+    if (r == SOCKET_ERROR) {
+        std::cerr << "closesocket(acitve) failed with error " << WSAGetLastError() << std::endl;
+        return 1;
+    }
+
+    // 연결을 기다리는 passive socket 을 닫는다.
+    r = closesocket(passiveSock);
+    if (r == SOCKET_ERROR) {
+        std::cerr << "closesocket(passive) failed with error " << WSAGetLastError() << std::endl;
+        return 1;
+    }
+
+    // Winsock 을 정리한다.
+    WSACleanup();
+    return 0;
+}
